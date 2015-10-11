509bcdcad7a4a70daff8145ca96d1611608fe108
JDVB <jorndevolder8@hotmail.com>
2015-10-11 15:11:41 +0200

    Modified EmailValidator-->can't create an account with an email address that's already taken

--- ReDoAir-web/src/main/java/com/realdolmen/air/validators/EmailValidator.java
+++ ReDoAir-web/src/main/java/com/realdolmen/air/validators/EmailValidator.java
@@ -1,7 +1,8 @@
-package validators;
+package com.realdolmen.air.validators;
 
 import com.realdolmen.redoair.ejb.UserRepository;
 
+import javax.enterprise.context.RequestScoped;
 import javax.faces.application.FacesMessage;
 import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
@@ -9,6 +10,7 @@ import javax.faces.validator.FacesValidator;
 import javax.faces.validator.Validator;
 import javax.faces.validator.ValidatorException;
 import javax.inject.Inject;
+import javax.inject.Named;
 import javax.persistence.PersistenceContext;
 import javax.persistence.PersistenceException;
 import java.util.regex.Matcher;
@@ -17,13 +19,13 @@ import java.util.regex.Pattern;
 /**
  * Created by JDOAX80 on 8/10/2015.
  */
-@FacesValidator("emailValidator")
+@Named
+@RequestScoped
 public class EmailValidator implements Validator {
 
     @Inject
     private UserRepository userRepository;
 
-    private static final String EMAIL_ALREADY_EXISTS = "This email address is already used.";
     private static final String EMAIL_PATTERN = "^[_A-Za-z0-9-]+(\\." +
             "[_A-Za-z0-9-]+)*@[A-Za-z0-9]+(\\.[A-Za-z0-9]+)*" +
             "(\\.[A-Za-z]{2,})$";
@@ -40,14 +42,17 @@ public class EmailValidator implements Validator {
                          Object value) throws ValidatorException {
         String email = value.toString();
         matcher = pattern.matcher(email);
-            if(!matcher.matches()){
-                FacesMessage msg =
-                        new FacesMessage("E-mail validation failed.",
-                                "Invalid E-mail format.");
-                msg.setSeverity(FacesMessage.SEVERITY_ERROR);
-                throw new ValidatorException(msg);
-            }
-
+        if (!matcher.matches()) {
+            FacesMessage msg =
+                    new FacesMessage("E-mail validation failed.",
+                            "Invalid E-mail format.");
+            msg.setSeverity(FacesMessage.SEVERITY_ERROR);
+            throw new ValidatorException(msg);
+        }
+        if(userRepository.getUserByEmail(email) != null) {
+            throw new ValidatorException(new FacesMessage(
+                    "There already exists an account for this email address!"));
+        }
     }
 
 }
